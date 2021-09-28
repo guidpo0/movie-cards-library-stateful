8685ba77dd74ba3134f50d20a0e17fee17869864
Gui Pereira <guilhermepdo1508@gmail.com>
2021-09-28 11:35:16 -0300

    Updates

---
+++ precache-manifest.763321fb9f9d13c76d6fad39345f784b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2708770cdda0d395a7bb2d492a05ac8",
+    "url": "/movie-cards-library-stateful/index.html"
+  },
+  {
+    "revision": "34276a543bfa68320101",
+    "url": "/movie-cards-library-stateful/static/css/main.e274d78f.chunk.css"
+  },
+  {
+    "revision": "86b1685af6dcbcbb1c69",
+    "url": "/movie-cards-library-stateful/static/js/2.6879c355.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/movie-cards-library-stateful/static/js/2.6879c355.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34276a543bfa68320101",
+    "url": "/movie-cards-library-stateful/static/js/main.57480d28.chunk.js"
+  },
+  {
+    "revision": "bdf93eebb3ecba7817db",
+    "url": "/movie-cards-library-stateful/static/js/runtime-main.2979b9ff.js"
+  },
+  {
+    "revision": "498ae78ab8b121e04dadf6e51342eeca",
+    "url": "/movie-cards-library-stateful/static/media/Appleseed_Alpha.498ae78a.jpg"
+  },
+  {
+    "revision": "0d8b80ecc214d904b30bbd706f681cc9",
+    "url": "/movie-cards-library-stateful/static/media/Final_Fantasy_Spirits_Within.0d8b80ec.jpg"
+  },
+  {
+    "revision": "7d6b4f9cc69800cbd7c5a4fd2f0aeec5",
+    "url": "/movie-cards-library-stateful/static/media/Ghost_In_The_Shell_2_0.7d6b4f9c.jpg"
+  },
+  {
+    "revision": "917daac0bfa6f2ab2ffd7f26bc5ac239",
+    "url": "/movie-cards-library-stateful/static/media/Kingsglaive_Final_Fantasy_XV.917daac0.jpg"
+  },
+  {
+    "revision": "77c10ef352abfe64f4a61da3e951519b",
+    "url": "/movie-cards-library-stateful/static/media/Resident_Evil_Vendetta.77c10ef3.jpg"
+  }
+]);
